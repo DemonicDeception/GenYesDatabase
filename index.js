ed73d8ddda784d0cf43e834791014d3e34d782ef
Hrittik Chatterjee <chatterjee12355@gmail.com>
2021-10-27 08:31:54 -0700

    Add project files.

---
+++ index.js
@@ -0,0 +1,22 @@
+var admin = require("firebase-admin");
+const path = require('path');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const wait = require('util').promisify(setTimeout);
+const port = 3000
+var serviceAccount = require("./DataBaseKeys.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/html/index.html'))
+})
+app.listen(port, () => {
+  console.log(`app listening at http://localhost:${port}`)
+})
